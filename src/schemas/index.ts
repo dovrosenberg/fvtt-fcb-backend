aa3c5aef2f35b1f52863dff1d65d56259ac1078b
Dov Rosenberg <dov@rosenbergmail.com>
2025-03-19 10:00:16 -0400

    bug fix

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,16 @@
 export * from './character';
 export * from './organization';
 export * from './location';
+
+import { FromSchema } from 'json-schema-to-ts';
+
+export const versionResponseSchema = {
+  type: 'object',
+  properties: {
+    version: { type: 'string', description: 'The current version of the backend' },
+  },
+  required: ['version']
+} as const;
+
+export type VersionOutput = FromSchema<typeof versionResponseSchema>;
+
